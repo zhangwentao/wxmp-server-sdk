959ab7a483d67de2e756bb8733bfe46ef60d0bc0
zhangwentao <taowenzhang@gmail.com>
2024-12-09 01:01:23 +0800

    feat: init

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const URL = {
+  jscode2session: 'https://api.weixin.qq.com/sns/jscode2session',
+  accessToken: 'https://api.weixin.qq.com/cgi-bin/token',
+  subscribeSend: 'https://api.weixin.qq.com/cgi-bin/message/subscribe/send'
+}
+
+module.exports = {
+  URL 
+}
---
+++ src/get-access-token.ts
@@ -0,0 +1,40 @@
+import { get } from 'https'
+import { URL } from './constants'
+const grant_type = 'client_credential'
+
+const getAccessToken = async (params: {
+  appid: string
+  secret: string
+}): Promise<{
+  access_token: string
+  expires_in: number
+}> => {
+  const { appid, secret } = params
+  return new Promise((resolve, reject) => {
+    get(
+      `${URL.accessToken}?grant_type=${grant_type}&appid=${appid}&secret=${secret}`,
+      (res) => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            const { access_token, expires_in } = parsedData
+            console.log(access_token, expires_in)
+            resolve({
+              access_token,
+              expires_in
+            });
+          } catch (e) {
+            reject(e.message);
+          }
+        });
+      }
+    )
+  })
+}
+
+module.exports = {
+  getAccessToken
+}
